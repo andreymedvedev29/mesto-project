5cdecd1eb326aeae45cf96f5ecd0c6f0319ab410
Andrei Medvedev <medvedevandrei29@yandex.ru>
2022-01-20 22:27:43 +0300

    js код разбит на пять модулей

--- src/components/index.js
+++ src/components/index.js
@@ -151,83 +151,8 @@ addPlaceForm.addEventListener('submit', handlePlaceFormSubmit);
 
 
 
-// Вынесем все необходимые элементы формы в константы
-const form = document.querySelector('.popup__form');
-const formInput = form.querySelector('.popup__item');
-const formError = form.querySelector(`.${formInput.id}-error`); 
 
-// Функция, которая добавляет класс с ошибкой
-const showInputError = (formElement, inputElement, errorMessage) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add('popup__item_type_error');
-  errorElement.textContent = errorMessage;
-  errorElement.classList.add('popup__item-error_activ');
-};
-
-// Функция, которая удаляет класс с ошибкой
-const hideInputError = (formElement, inputElement) => {
-  const errorElement = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.remove('popup__item_type_error');
-  errorElement.classList.add('popup__item-error_activ');
-  errorElement.textContent = '';
-};
-
-//Функция валидации полей
-const checkInputValidity = (formElement, inputElement) => {
-  if (!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, inputElement.validationMessage);
-  } else {
-    hideInputError(formElement, inputElement);
-  }
-};
-
-//Функция ищет хлтя бы одно невалидное поле
-const hasInvalidInput = (inputList) => {
-  return inputList.some((inputElement) => {
-    return !inputElement.validity.valid;
-  })
-};
-
-
-//Функция переключения кнопки актив/неактив
-const toggleButtonState = (inputList, buttonElement) => {
-  // Если есть хотя бы один невалидный инпут
-  if (hasInvalidInput(inputList)) {
-    // сделай кнопку неактивной
-    buttonElement.classList.add('popup__button-activ');
-  } else {
-        // иначе сделай кнопку активной
-    buttonElement.classList.remove('popup__button-activ');
-  }
-};
-
-
-//Функция добавляет слушателя всем полям внутри формы
-const setEventListeners = (formElement) => {
-  const inputList = Array.from(formElement.querySelectorAll('.popup__item'));
-  const buttonElement = formElement.querySelector('.popup__button');
-  // чтобы проверить состояние кнопки в самом начале
-  toggleButtonState(inputList, buttonElement);
-  inputList.forEach((inputElement) => {
-    inputElement.addEventListener('input', function () {
-      checkInputValidity(formElement, inputElement);
-      // чтобы проверять его при изменении любого из полей
-      toggleButtonState(inputList, buttonElement);
-    });
-  });
-};
-
-//Функция добавляет обработчик  всем формам
-const enableValidation = () => {
-  const formList = Array.from(document.querySelectorAll('.popup__form'));
-  formList.forEach((formElement) => {
-    formElement.addEventListener('submit', function (evt) {
-      evt.preventDefault();
-    });
-    setEventListeners(formElement)
-  });
-};
-
+import { enableValidation } from './validate.js'
 enableValidation();
 
 
